3bc1b159f48d055f1d8a4c14406d2decad582a2b
PhilGru <philipp1805@gmx.net>
2023-05-03 00:17:52 +0200

    add info

---
+++ docs/_app/immutable/entry/_page.svelte.1824d985.js
@@ -0,0 +1,29 @@
+import{S as W,i as b,s as A,k as m,q as o,a as H,l as v,m as _,r,h as e,c as K,n as C,b as p,G as i,H as P}from"../chunks/index.e8d42111.js";function G(V){let n,d,f,a,u,x,k,E,c,l,S;return{c(){n=m("h1"),d=o("Welcome to SvelteKit"),f=H(),a=m("p"),u=o("Visit "),x=m("a"),k=o("kit.svelte.dev"),E=o(" to read the documentation"),c=H(),l=m("p"),S=o(`1 12x1
+    1 1x2
+    2 8x1
+    3 12x1
+    3 1x2
+    4 11x1
+    4 2x2
+    5 6x1
+    6 12x1
+    6 1x2
+    7 1x1
+    8 9x1
+    9 36x1
+    9 3x3
+    9 14x2`),this.h()},l(t){n=v(t,"H1",{});var s=_(n);d=r(s,"Welcome to SvelteKit"),s.forEach(e),f=K(t),a=v(t,"P",{});var h=_(a);u=r(h,"Visit "),x=v(h,"A",{href:!0});var q=_(x);k=r(q,"kit.svelte.dev"),q.forEach(e),E=r(h," to read the documentation"),h.forEach(e),c=K(t),l=v(t,"P",{});var y=_(l);S=r(y,`1 12x1
+    1 1x2
+    2 8x1
+    3 12x1
+    3 1x2
+    4 11x1
+    4 2x2
+    5 6x1
+    6 12x1
+    6 1x2
+    7 1x1
+    8 9x1
+    9 36x1
+    9 3x3
+    9 14x2`),y.forEach(e),this.h()},h(){C(x,"href","https://kit.svelte.dev")},m(t,s){p(t,n,s),i(n,d),p(t,f,s),p(t,a,s),i(a,u),i(a,x),i(x,k),i(a,E),p(t,c,s),p(t,l,s),i(l,S)},p:P,i:P,o:P,d(t){t&&e(n),t&&e(f),t&&e(a),t&&e(c),t&&e(l)}}}class w extends W{constructor(n){super(),b(this,n,null,G,A,{})}}export{w as default};
